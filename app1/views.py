dcb154046d3fdac168821882e214b4920fa05a63
Contino Emiliano <continoemiliano@gmail.com>
2022-10-15 18:58:42 -0300

    Cree template de eliminar y editar - Cargue imagenes y modifique descripciones de las mismas

--- app1/views.py
+++ app1/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-# from app1.forms import ChefFormulario
 from app1.models import Chef
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
 def index(request):
     return render (request, 'app1/index.html')
-
-
-# def ver_chef(request):
-#     chef = Chef.objects.all
-#     return render (request, 'app1/ver_chef.html', {'chef' : chef})
 
 class ListaChef(ListView):
     model = Chef
